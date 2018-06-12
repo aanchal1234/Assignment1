646cd0616b4a98dfc1031f0626a62a47ab8c5ce3
aanchal1234 <aanchalbudhiraja955@gmail.com>
2018-06-12 01:11:59 -0700

    File updated

---
+++ demo.py
@@ -0,0 +1,22 @@
+print ("hello world!")
+print("computer is an electronic device!")
+x=5
+print(x)
+print(type(x))
+
+x=1.5
+print(x)
+print(type(x))
+
+x='a'
+print(x)
+print(type(x))
+
+x='abcdef'
+print(x)
+print(type(x))
+
+x=5+10J
+x=x+5
+print(x)
+print(type(x))
