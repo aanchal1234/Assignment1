ed687e5c8881020828b3e7f70ff74eda51ee40b3
aanchal1234 <40165591+aanchal1234@users.noreply.github.com>
2018-07-12 05:40:17 -0700

    Add files via upload

---
+++ weather api.py
@@ -0,0 +1,30 @@
+#1WEATHER API CALL
+# import requests
+# # from pprint import pprint
+#
+# api_address="http://api.openweathermap.org/data/2.5/weather?appid=4dc0d91cd6295f0fec16a816bc60d91f&q="
+# city=input("Enter your city: ")
+# url=api_address+city
+# json_data=requests.get(url).json()
+# print(json_data)
+# temp=json_data['main']['temp']
+# wind_speed=json_data['wind']['speed']
+# latitude=json_data['coord']['lat']
+# longitude=json_data['coord']['lon']
+# humidity=json_data['main']['humidity']
+# description=json_data['weather'][0]['description']
+# temp_max=json_data['main']['temp_max']
+#
+# print('temperature:{}degree calcius'.format(temp))
+# print('wind_speed:{}m/s'.format(wind_speed))
+# print('latitude:{}'.format(latitude))
+# print('longitude:{}'.format(longitude))
+# print('description:{}'.format(description))
+# print('humidity:{}'.format(humidity))
+
+#2
+from tkinter import *
+
+
+
+
